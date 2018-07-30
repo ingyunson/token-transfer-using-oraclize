1901849154169599bd2ba1e5964d8fa22e267366
Ingyun Son <ingyunson@gmail.com>
2018-07-30 19:31:02 +0900

    feat: Edit youtube_viewcount.sol add youtube address when deploy the contract
    When the contract deploy, you can use youtube address to send token.
    
    Input youtube video address on "videoaddress" dependent when you deploy the contract.

--- youtube_viewcount.sol
+++ youtube_viewcount.sol
@@ -1,22 +1,43 @@
 pragma solidity ^0.4.0;
-
 import "github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
-
 contract YoutubeViews is usingOraclize {
-
     string public viewsCount;
     bytes32 public oraclizeID;
-
     event NewYoutubeViewsCount(string views);
-
-    function YoutubeViews() public payable {
-        oraclizeID = oraclize_query("URL", 'html(https://www.youtube.com/watch?v=9bZkp7q19f0).xpath(//*[contains(@class, "watch-view-count")]/text())');
+    function YoutubeViews(string videoaddress) public payable {
+        string memory query = strConcat('html(',videoaddress,').xpath(//*[contains(@class, "watch-view-count")]/text())');
+        oraclizeID = oraclize_query("URL", query);
     }
-
     function __callback(bytes32 myid, string result) public {
         if (msg.sender != oraclize_cbAddress()) revert();
         viewsCount = result;
         NewYoutubeViewsCount(viewsCount);
         // do something with viewsCount. like tipping the author if viewsCount > X?
+    }
+    function strConcat(string _a, string _b, string _c, string _d, string _e) internal pure returns (string){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory _bc = bytes(_c);
+        bytes memory _bd = bytes(_d);
+        bytes memory _be = bytes(_e);
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
+        bytes memory babcde = bytes(abcde);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
+        return string(babcde);
+    }
+    
+    function strConcat(string _a, string _b, string _c, string _d) internal pure returns (string) {
+        return strConcat(_a, _b, _c, _d, "");
+    }
+    function strConcat(string _a, string _b, string _c) internal pure returns (string) {
+        return strConcat(_a, _b, _c, "", "");
+    }
+    function strConcat(string _a, string _b) internal pure returns (string) {
+        return strConcat(_a, _b, "", "", "");
     }
 }
