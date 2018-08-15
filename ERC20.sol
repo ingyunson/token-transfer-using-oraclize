be67acf43dc18925e91e5f327723a1a770b945c8
Ingyun Son <ingyunson@gmail.com>
2018-08-15 15:19:41 +0900

    Refactor some codes

--- ERC20.sol
+++ ERC20.sol
@@ -13,25 +13,44 @@ contract SimpleToken is StandardToken {
   string public constant name = "Token";
   string public constant symbol = "TOK";
   uint8 public constant decimals = 18;
-
   uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+  address public owner;
+  mapping(address => bool) public allowedControllers;
+  
+/*  modifier onlyOwner() {
+      require(msg.sender == owner);
+      _;
+  }
+  
+    function addControllers (address controller) onlyOwner {
+        allowedControllers[controller]=true;
+    }   
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
+    modifier byControllers() {
+        require(allowedControllers[msg.sender] == true);
+        _;
+    }
    */
   constructor() public payable {
     totalSupply_ = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
     emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+    owner = msg.sender;
+    //addControllers(msg.sender);
   }
   
-  function approvecontract (address _spender, uint256 _value) public returns (bool) {
-      allowed[this][_spender] = _value;
-      emit Approval(this, _spender, _value);
-      return true;
-  }
+  function transfercontract (address _to, uint256 _value) public returns (bool) {
+    require(_value <= balances[this]);
+    require(_value <= allowed[this][msg.sender]);
+    require(_to != address(0));
 
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    balances[this] = balances[this].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(this, _to, _value);
+    return true;
+  }
+  
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
     require(_value <= balances[_from]);
     require(_to != address(0));
 
@@ -40,6 +59,15 @@ contract SimpleToken is StandardToken {
     emit Transfer(_from, _to, _value);
     return true;
   }
+  
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_value <= balances[msg.sender]);
+    require(_to != address(0));
 
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
   
 }
