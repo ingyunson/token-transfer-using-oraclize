ce057cd437febbd48a1af86cd6658a37f7e3938c
Ingyun Son <ingyunson@gmail.com>
2018-08-25 18:51:28 +0900

    Add : mapping protocol that shows recent transaction of the address

--- youtube_viewcount.sol
+++ youtube_viewcount.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.24;
 
 import "github.com/oraclize/ethereum-api/oraclizeAPI_0.5.sol";
-import "github.com/ingyunson/token-transfer-using-orzclize/ERC20.sol";
+import "./ERC20.sol";
+
 
 /**
  * @title SimpleToken
@@ -9,38 +10,34 @@ import "github.com/ingyunson/token-transfer-using-orzclize/ERC20.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract OraclizeTestToken is usingOraclize, StandardToken {
+contract YoutubeCounter is usingOraclize, StandardToken {
 
-    string public name; // solium-disable-line uppercase
-    string public symbol; // solium-disable-line uppercase
-    uint8 public decimals; // solium-disable-line uppercase
-    uint256 public INITIAL_SUPPLY;
     string public viewsCount;
-    bytes32 public oraclizeID;
+    bytes32 oraclizeID;
     uint public amount;
     uint public balance;
-    address public owner;
+    address owner;
     address public beneficiary;
     uint public PayPerView;
     string public videoaddress;
+    mapping(address => transaction) public transaction_rec;
 
     event NewYoutubeViewsCount(string views);
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
-  function OraclizeTestToken(string _name, string _symbol, uint8 _decimals, uint256 _initial_supply) public {
-      name = _name;
-      symbol = _symbol;
-      decimals = _decimals;
-      INITIAL_SUPPLY = _initial_supply * (10 ** uint256(decimals));
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[this] = INITIAL_SUPPLY;
-    emit Transfer(0x0, this, INITIAL_SUPPLY);
-  }
+    struct transaction {
+        uint blocknum;
+        uint blocktime;
+        uint viewrate;
+        uint amount_of_transfer;
+        string targetaddress;
+        
+    }
     
+   function YoutubeCounter() {
+       owner = msg.sender;
+   }
+   
     function YoutubeViews(string _videoaddress, address _beneficiary, uint _PayPerView) public payable {
-        owner = msg.sender;
         beneficiary = _beneficiary;
         PayPerView = _PayPerView;
         videoaddress = _videoaddress;
@@ -63,14 +60,16 @@ contract OraclizeTestToken is usingOraclize, StandardToken {
         }
     }
     
-    function () public payable {
-        require(beneficiary != address(0));
-        require(amount <= balances[this]);
-    
-        // SafeMath.sub will throw if there is not enough balance.
-        balances[this] = balances[this].sub(amount);
-        balances[beneficiary] = balances[beneficiary].add(amount);
-        emit Transfer(this, beneficiary, amount);
+    function ethertransfer() payable {
+        address target = beneficiary;
+        uint transfer_amount = amount;
+        target.transfer(transfer_amount);
+        
+        transaction_rec[beneficiary].blocknum = block.number;
+        transaction_rec[beneficiary].blocktime = block.timestamp;
+        transaction_rec[beneficiary].viewrate = PayPerView;
+        transaction_rec[beneficiary].amount_of_transfer = amount;
+        transaction_rec[beneficiary].targetaddress = videoaddress;
     }
     
 
