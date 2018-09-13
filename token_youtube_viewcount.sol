f6ffba3f07bae8644638bf97a28fc20064a368e5
Ingyun Son <ingyunson@gmail.com>
2018-09-13 23:57:07 +0900

    Fix: fix ERC20 import & make interface for using oraclize functions

--- token_youtube_viewcount.sol
+++ token_youtube_viewcount.sol
@@ -5,14 +5,18 @@ import "./ERC20.sol";
 
 
 interface Interface {
-        function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
+    function strConcat(string _a, string _b, string _c, string _d, string _e) returns (string);
+    function strConcat(string _a, string _b, string _c, string _d) returns (string);
+    function strConcat(string _a, string _b, string _c) returns (string);
+    function strConcat(string _a, string _b) returns (string);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
 }
 
-contract YoutubeViews is usingOraclize, StandardToken {
+contract YoutubeViews is usingOraclize, ERC20 {
 
     string viewsCount;
     bytes32 oraclizeID;
-    uint amount;
+    uint public amount;
     uint balance;
     address owner;
     address beneficiary;
@@ -39,9 +43,9 @@ contract YoutubeViews is usingOraclize, StandardToken {
         uint amount_of_transfer;
         string targetaddress;
     }
-
-    Interface Token = Interface(tokenaddress);
     
+    Interface Token = Interface(tokenaddress);
+
     constructor() {
         owner = msg.sender;
     }
@@ -96,9 +100,7 @@ contract YoutubeViews is usingOraclize, StandardToken {
         }
     }
 
-
-
-    function stringToUint(string s) internal pure returns (uint result) {
+function stringToUint(string s) internal pure returns (uint result) {
         bytes memory b = bytes(s);
         uint i;
         result = 0;
@@ -110,31 +112,4 @@ contract YoutubeViews is usingOraclize, StandardToken {
         }
     }
 
-    function strConcat(string _a, string _b, string _c, string _d, string _e) internal pure returns (string){
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        bytes memory _bc = bytes(_c);
-        bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-        bytes memory babcde = bytes(abcde);
-        uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-        return string(babcde);
-    }
-    
-    function strConcat(string _a, string _b, string _c, string _d) internal pure returns (string) {
-        return strConcat(_a, _b, _c, _d, "");
-    }
-    function strConcat(string _a, string _b, string _c) internal pure returns (string) {
-        return strConcat(_a, _b, _c, "", "");
-    }
-    function strConcat(string _a, string _b) internal pure returns (string) {
-        return strConcat(_a, _b, "", "", "");
-    }
 }
-
