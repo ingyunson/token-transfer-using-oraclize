aa94a83e1fac1ec3d4bd12ea7d9ffb5c716211ba
Ingyun Son <ingyunson@gmail.com>
2018-09-14 13:06:47 +0900

    Refactor: refactor some codes
    * make interface as Interface which functions in oraclize

--- token_timer_youtube_viewcount.sol
+++ token_timer_youtube_viewcount.sol
@@ -5,9 +5,11 @@ import "./ERC20.sol";
 
 
 interface Interface {
-        function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
-        function transfer(address _to, uint256 _value) public returns (bool);
-        function transfercontract (address _to, uint256 _value) public returns (bool);
+    function strConcat(string _a, string _b, string _c, string _d, string _e) returns (string);
+    function strConcat(string _a, string _b, string _c, string _d) returns (string);
+    function strConcat(string _a, string _b, string _c) returns (string);
+    function strConcat(string _a, string _b) returns (string);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool);
 }
 
 contract YoutubeViews is usingOraclize, StandardToken {
@@ -127,8 +129,6 @@ contract YoutubeViews is usingOraclize, StandardToken {
         }
     }
 
-
-
     function stringToUint(string s) internal pure returns (uint result) {
         bytes memory b = bytes(s);
         uint i;
@@ -139,33 +139,6 @@ contract YoutubeViews is usingOraclize, StandardToken {
                 result = result * 10 + (c - 48);
             }
         }
-    }
-
-    function strConcat(string _a, string _b, string _c, string _d, string _e) internal pure returns (string){
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        bytes memory _bc = bytes(_c);
-        bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-        bytes memory babcde = bytes(abcde);
-        uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-        return string(babcde);
-    }
-    
-    function strConcat(string _a, string _b, string _c, string _d) internal pure returns (string) {
-        return strConcat(_a, _b, _c, _d, "");
-    }
-    function strConcat(string _a, string _b, string _c) internal pure returns (string) {
-        return strConcat(_a, _b, _c, "", "");
-    }
-    function strConcat(string _a, string _b) internal pure returns (string) {
-        return strConcat(_a, _b, "", "", "");
     }
 }
 
